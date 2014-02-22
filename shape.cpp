7599af3268533277e5624847b871a53c28fb3afd
Enzhaev Ivan <8observer8@gmail.com>
2014-02-22 23:29:41 +0300

    add shape.cpp

---
+++ shape.cpp
@@ -0,0 +1,29 @@
+#include "shape.h"
+
+/*
+Shape::Shape() :
+    mPerimeter(0.0),
+    mArea(0.0)
+{
+}
+*/
+
+Shape::Shape()
+{
+}
+
+Shape::~Shape()
+{
+}
+
+/*
+double Shape::perimeter() const
+{
+    return mPerimeter;
+}
+
+double Shape::area() const
+{
+    return mArea;
+}
+*/
