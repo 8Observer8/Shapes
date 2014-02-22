48c362aea5e277f7e0357c443d5218e0fa607847
Enzhaev Ivan <8observer8@gmail.com>
2014-02-22 10:45:06 +0300

    Demonstration of polymorphous functions: calcPerimeter() and calcArea()

--- shape.cpp
+++ shape.cpp
@@ -1,20 +1,20 @@
 #include "shape.h"
-
+ 
 Shape::Shape() :
     mPerimeter(0.0),
     mArea(0.0)
 {
 }
-
+ 
 Shape::~Shape()
 {
 }
-
+ 
 double Shape::perimeter() const
 {
     return mPerimeter;
 }
-
+ 
 double Shape::area() const
 {
     return mArea;
